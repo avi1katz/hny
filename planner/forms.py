c81ce38e9a959daeadc64fa0c7c20aec2d06af58
avi <avi1katz@gmail.com>
2022-01-26 06:56:32 -0600

    form cleanup

--- planner/forms.py
+++ planner/forms.py
@@ -2,5 +2,8 @@ from django import forms
 
 
 class TaskForm(forms.Form):
-    description = forms.CharField(label='Task Description', max_length=15)
-    minutes_to_complete = forms.IntegerField(label='Minutes to Complete', max_value=120, min_value=5, initial=30)
+    description = forms.CharField(label='Task Description', max_length=30,
+                                  widget=forms.TextInput(attrs={'placeholder': 'task description...',
+                                                                'class': 'form-control'}))
+    minutes_to_complete = forms.IntegerField(label='Minutes to Complete', max_value=120, min_value=5, initial=30,
+                                             widget=forms.NumberInput(attrs={'class': 'form-control'}))
