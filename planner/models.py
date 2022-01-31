62429d2bfb54ac80dbc8a9ed0af24daea923fc6d
avi <akatz@iinktech.com>
2022-01-30 21:23:36 -0600

    Update Agenda Model to create default items on save

--- planner/models.py
+++ planner/models.py
@@ -23,12 +23,23 @@ class Agenda(models.Model):
     def __str__(self):
         return self.date.strftime("%d-%b-%y")
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)  # Call the "real" save() method.
+        if self.parent is None and not self.agenda_items.exists():
+            self.agenda_items.create(start_time=self.default_begin_time,
+                                     task=Task.objects.create(description="Plan day", minutes_to_complete=15))
+            self.agenda_items.create(start_time=self.default_end_time,
+                                     task=Task.objects.create(description="Head to Bed", minutes_to_complete=15))
+
 
 # Agenda_Item - agenda_id, start_time, task_id
 class AgendaItem(models.Model):
     agenda = models.ForeignKey(Agenda, on_delete=models.CASCADE, related_name='agenda_items')
     start_time = models.TimeField()
     task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='agenda_items')
+
+    class Meta:
+        ordering = ['start_time', ]
 
     def __str__(self):
         return f'Agenda: {self.agenda}, start_time: {self.start_time}, task: {self.task}'
